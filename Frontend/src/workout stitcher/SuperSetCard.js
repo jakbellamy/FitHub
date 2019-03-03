18c6de03369113a81b3142222cc23cdbced6c5b5
Jakob Bellamy <jakbellamy@gmail.com>
2019-03-03 12:46:13 -0600

    drop is actually finding its target now, but drop is drag index plus one so wrong items

--- Frontend/src/workout stitcher/SuperSetCard.js
+++ Frontend/src/workout stitcher/SuperSetCard.js
@@ -6,7 +6,7 @@ export default class SuperSetCard extends Component {
       <div className="draggable-item">
         <h4 style={{textAlign: 'center', borderStyle: 'solid'}}>{this.props.name}</h4>
         <div style={{display: 'flex', justifyContent: 'center'}}>
-        <p>{this.props.sets.excercises.map(exercise => <p key={exercise._id}>{exercise.reps}X {exercise.name}</p> )}</p>
+        <span>{this.props.sets.excercises.map(exercise => <p key={exercise._id}>{exercise.reps}X {exercise.name}</p> )}</span>
         </div>
 
       </div>
--- Frontend/src/workout stitcher/SupersetsLib.js
+++ Frontend/src/workout stitcher/SupersetsLib.js
@@ -19,43 +19,50 @@ export default class SupersetsLib extends Component {
   render() {
     console.log(this.props)
     return (
-      <div className='superset-library' style={{ display: 'flex', justifyContent: 'stretch', marginTop: '50px', marginRight: '50px' }}> 
+      <div style={{overflow:'auto'}}>
+        <div style={{ display: 'flex', justifyContent: 'stretch', marginTop: '50px', marginRight: '50px', height: '1000px', width: '1000px', overflow: 'auto' }}>
 
-        <div style={this.groupStyle}>
-          <Container 
-            groupName='things' 
-            behaviour="copy" 
-            getChildPayload={i => this.props.supersets[i]}
-            onDrop={e => this.props.setSupersets(ApplyDrag(this.props.supersets, e))}>
-              {/* <p style={{textAlign: 'center'}}>Supersets Library</p> */}
-              {
-                this.props.supersets.map(superset => {
-                 return (
-                   <Draggable key={superset._id}>
-                      <SuperSetCard key={superset._id} {...superset} />
-                    </Draggable>  
-                 );
-                })
-              }
-          </Container>
-        </div>
-
-        <div style={this.groupStyle}>
-          <Container 
-            groupName='things'
-            getChildPayload={i => this.props.newWorkoutSets[i]} 
-            onDrop={e => this.props.setNewWorkoutSets(ApplyDrag(this.props.newWorkoutSets, e))}>
-              {/* <p style={{textAlign: 'center'}}>New Workout</p> */}
-              {
-                this.props.newWorkoutSets.map(superset => {
+            <Container 
+              style={this.groupStyle}
+              groupName='supersets' 
+              behaviour="copy" 
+              // dragClass="card-ghost"
+              // dropClass="card-ghost-drop"
+              getChildPayload={i => this.props.supersets[i]}
+              onDrop={e => this.props.setSupersets(ApplyDrag(this.props.supersets, e))}>
+                <p style={{textAlign: 'center'}}>Supersets Library</p>
+                {
+                  this.props.supersets.map(superset => {
                   return (
                     <Draggable key={superset._id}>
-                       <SuperSetCard key={superset._id} {...superset} />
+                        <SuperSetCard key={superset._id} {...superset} />
+                      </Draggable>  
+                  );
+                  })
+                }
+            </Container>
+
+            <Container 
+              style={this.groupStyle}
+              groupName='supersets'
+              dragClass="card-ghost"
+              dropClass="card-ghost-drop"
+              getChildPayload={i => this.props.newWorkoutSets[i]} 
+              // behaviour="drop-zone"
+              onDrop={e => this.props.setNewWorkoutSets(ApplyDrag(this.props.newWorkoutSets, e))}>
+              <p style={{textAlign: 'center'}}>New Workout</p>
+                {
+                  this.props.newWorkoutSets.map(superset => {
+                    return (
+                    <Draggable key={superset._id}>
+                      <SuperSetCard key={superset._id} {...superset} />
                     </Draggable>  
                   )
                 })
               }
-          </Container>
+            </Container>
+
+          
         </div>
       </div>
     )
